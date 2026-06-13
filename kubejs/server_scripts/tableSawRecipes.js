43c6a28dfae4ec301b61ee39f9035240e20d3a30
purplepentagons <197023143+purplepentagons@users.noreply.github.com>
2026-06-12 22:36:20 -0400

    add woodcutting to table saw

--- kubejs/server_scripts/tableSawRecipes.js
+++ kubejs/server_scripts/tableSawRecipes.js
@@ -1,4 +1,16 @@
 ServerEvents.recipes(event => {
+    // bamboo, crimson, warped not included because the log doesn't follow the log naming pattern
+    const planksWoodList = [
+        "oak",
+        "spruce",
+        "birch",
+        "jungle",
+        "acacia",
+        "cherry",
+        "dark_oak",
+        "mangrove"
+    ]
+
     event.remove({
         type: "minecraft:crafting_shaped",
         output: "#minecraft:stairs",
@@ -29,4 +41,25 @@ ServerEvents.recipes(event => {
     })})
 
     event.stonecutting("minecraft:diamond", "kubejs:pure_diamond");
+
+    event.stonecutting("8x minecraft:stick", "#minecraft:logs");
+
+    planksWoodList.forEach((woodType) => {
+        event.stonecutting(`4x minecraft:${woodType}_planks`, `#minecraft:${woodType}_logs`);
+    })
+
+    event.stonecutting("4x minecraft:warped_planks", "#minecraft:warped_stems");
+    event.stonecutting("4x minecraft:crimson_planks", "#minecraft:crimson_stems");
+    event.stonecutting("2x minecraft:bamboo_planks", "#minecraft:bamboo_blocks");
+    event.stonecutting("2x minecraft:stick", "minecraft:bamboo_planks");
+
+    // anything after this point avoids the logs
+    planksWoodList = planksWoodList.concat(
+        ["crimson", "warped", "bamboo"]
+    )
+
+    planksWoodList.forEach((woodType) => {
+        event.stonecutting(`1x minecraft:${woodType}_stairs`, `minecraft:${woodType}_planks`);
+        event.stonecutting(`2x minecraft:${woodType}_slab`, `minecraft:${woodType}_planks`);
+    })
 })
